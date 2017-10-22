97f3d5c5d3a86e772dcae289ed9b9f83adfe9797
bluebear94 <uruwi@protonmail.com>
2017-10-22 00:33:36 -0400

    Add S and X

--- lt/genglyphs.py
+++ lt/genglyphs.py
@@ -6,17 +6,24 @@ CONSONANTS = "PTKSFNMHZYCXLWGQDBAIEUROV"
 
 behaviours = {
   "K": {
-    "mat": psMat.compose(
-      psMat.scale(0.4),
-      psMat.translate(550, 0)),
+    "scale": 0.4,
+    "offset": (550, 0),
     "dia": "ltd01"
   },
   "T": {
-    "mat": psMat.compose(
-      psMat.scale(64.0 / 80.0),
-      psMat.translate(1000.0 * 16.0 / 80.0 / 2.0, 0)
-    ),
+    "scale": 64.0 / 80.0,
+    "offset": (1000.0 * 16.0 / 80.0 / 2.0, 0),
     "dia": "ltd02"
+  },
+  "S": {
+    "scale": 1.0,
+    "offset": (0, 0),
+    "dia": "ltd03"
+  },
+  "X": {
+    "scale": 0.9,
+    "offset": (-40, 0),
+    "dia": "ltd04"
   }
 }
 
@@ -32,14 +39,19 @@ for c in CONSONANTS:
       font.copy()
       font.selection.select(glyph)
       font.paste()
-      mat = beh["mat"]
-      print(mat)
-      font.transform(mat)
-      glyph.changeWeight(15)
+      scale = beh["scale"]
+      offset = beh["offset"]
+      font.transform(psMat.compose(
+        psMat.scale(scale),
+        psMat.translate(offset[0], offset[1])
+      ))
+      glyph.changeWeight(25 * (1 - scale))
       font.selection.select(beh["dia"])
       font.copy()
       font.selection.select(glyph)
       font.pasteInto()
+      glyph.width = 1000;
+      glyph.vwidth = 1000;
   # Generate double glyphs
 
 font.save(sys.argv[2])
